ce034b7b2aaed637959a8a1e4f27c336bb47a533
Antonio Jack <ict2jack@gmail.com>
2020-04-13 18:07:57 +0700

    remove sms pending type

--- app/invitation/type-invitation.ts
+++ app/invitation/type-invitation.ts
@@ -1,18 +1,9 @@
-import type {
-  InvitationSenderAgencyDetail,
-  AgentKeyDelegationProof,
-  InvitationSenderDetail,
-} from '../sms-pending-invitation/type-sms-pending-invitation'
-
 export type InvitationPayload = {
   senderEndpoint: string
   requestId: string
-  // senderAgentKeyDelegationProof: AgentKeyDelegationProof
   senderName: string
   senderDID: string
   senderLogoUrl: string
   senderVerificationKey: string
   targetName: string
-  senderDetail: InvitationSenderDetail
-  senderAgencyDetail: InvitationSenderAgencyDetail
 }
