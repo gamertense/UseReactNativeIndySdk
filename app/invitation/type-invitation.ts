05c433e4c686854d63a085a443689da27bb1837e
Antonio Jack <ict2jack@gmail.com>
2020-04-17 22:59:29 +0700

    remove unused type declaration

--- app/invitation/type-invitation.ts
+++
@@ -1,9 +0,0 @@
-export type InvitationPayload = {
-  senderEndpoint: string
-  requestId: string
-  senderName: string
-  senderDID: string
-  senderLogoUrl: string
-  senderVerificationKey: string
-  targetName: string
-}
